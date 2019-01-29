41f4c04d2f1085139a9c0cee2f971ce8753681fa
Flameri <arto.lindgren1@gmail.com>
2019-01-29 10:02:13 +0200

    Initialized project and ran Ef core power reverse engineer

---
+++ BankDbApi/BankDbApi/Models/Bank.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankDbApi.Models
+{
+    public partial class Bank
+    {
+        public Bank()
+        {
+            Account = new HashSet<Account>();
+        }
+
+        public long Id { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; }
+        [StringLength(10)]
+        public string BIC { get; set; }
+
+        [InverseProperty("Bank")]
+        public virtual ICollection<Account> Account { get; set; }
+    }
+}
---
+++ BankDbApi/BankDbApi/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankDbApi.Models
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Account = new HashSet<Account>();
+        }
+
+        public long Id { get; set; }
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [StringLength(50)]
+        public string LastName { get; set; }
+        public long? BankId { get; set; }
+        public string Password { get; set; }
+
+        [InverseProperty("Customer")]
+        public virtual ICollection<Account> Account { get; set; }
+    }
+}
