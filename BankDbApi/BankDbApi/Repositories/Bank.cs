a7e72177f166dfa97f19488017427126f84fb91e
1701542-Saimia <anttiyleinen@hotmail.com>
2019-01-29 11:02:30 +0200

    Added bank

--- BankDbApi/BankDbApi/Repositories/Bank.cs
+++ BankDbApi/BankDbApi/Repositories/Bank.cs
@@ -1,11 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using BankDbApi.Models;
 
 namespace BankDbApi.Repositories
 {
-    public class Bank
+    public class Bank: IBank
     {
+        private readonly BankdbContext _context;
+
+        public Bank(BankdbContext context)
+        {
+            _context = context;
+        }
+
+        //Create 
+
+        public Bank Create(Bank bank)
+        {
+            _context.Add(bank);
+            _context.SaveChanges();
+            return bank;
+        }
+       
+        //list
+
+        public List<Bank> Read()
+        {
+            // mitä tähän?
+            return null;
+        }
+
+        public Bank Read(int id)
+        {
+
+            return null;
+
+        }
+
+        public Bank Update(int id, Bank bank)
+        {
+            _context.Update(bank);
+            _context.SaveChanges();
+            return bank;
+        }
+
+        public void Delete(int id)
+        {
+            
+            _context.Account.Remove(id);// ?????
+            _context.SaveChanges();
+            
+        }
+
     }
 }
