01a0b02cf9d6da9d44dd3d7febfc289ef7984a6f
pekkamakkonen <pek.makkonen@gmail.com>
2019-01-31 09:52:05 +0200

    GET transactions

---
+++ BankDbApi/BankDbApi/Controllers/TransactionsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankDbApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly ITransactionService _transactionService;
+
+        public TransactionsController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        // GET api/transactions
+        [HttpGet]
+        public ActionResult<List<Transaction>> GetTransactions()
+        {
+            return new JsonResult(_transactionService.Read());
+        }
+    }
+}
